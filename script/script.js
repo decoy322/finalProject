3d7f5c257ee4c1377c671f19bfc5de5ac874f6af
Roman <36533510+decoy322@users.noreply.github.com>
2021-12-20 00:32:47 +0200

    Update script.js

--- script/script.js
+++ script/script.js
@@ -230,14 +230,6 @@ $(document).ready(function() {
 
 /*404-error*/
 
-
-var tl = new TimelineMax();
-
-tl.add(function(){
-  animateCopy();
-  blinkHandle();
-}, 0.2)
-
 function animateCopy() {
   mySplitText.split({type:"chars, words"})
   splitTextTimeline.staggerFrom(mySplitText.chars, 0.001, {autoAlpha:0, ease:Back.easeInOut.config(1.7), onComplete: function(){
